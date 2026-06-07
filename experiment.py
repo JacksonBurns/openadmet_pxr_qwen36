55465902c5933b5f4dc8fb61667abc51c22684f1
JacksonBurns <jwburns@mit.edu>
2026-06-06 21:25:21 -0400

    Revert "Exp 11: Push center to 3.85"
    This reverts commit c6a019f34f1e9154650dd3da07280ced40b4903d.

--- experiment.py
+++ experiment.py
@@ -646,7 +646,7 @@ def evaluate_model(model, test=None):
     uncertainty_scale = np.clip(pred_std / 0.28, 0.3, 2.5)
 
     from math import exp
-    gaussian = np.array([exp(-0.5 * ((p - 3.85) / 0.5) ** 2) for p in final_pred])
+    gaussian = np.array([exp(-0.5 * ((p - 3.75) / 0.5) ** 2) for p in final_pred])
     correction = -0.52 * gaussian * uncertainty_scale
     final_pred = final_pred + correction
 
