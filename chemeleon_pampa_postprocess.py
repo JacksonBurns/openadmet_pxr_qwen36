8ca62e64deb567f8bc56c12fafd3b08e50f6d44d
JacksonBurns <jwburns@mit.edu>
2026-06-09 13:18:32 -0400

    delete failed pampa work

--- chemeleon_pampa_postprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    # read each predicton file, rename the columns, write
-    for dset in ("test", "test_phase1", "train"):
-        df = pd.read_csv(f"{dset}_chemeleon_pampa_preds_individual.csv")
-        df = df.rename(columns={"Y_model_0": "model_0_pampa_prob", "Y_model_1": "model_1_pampa_prob", "Y_model_2": "model_2_pampa_prob", "Y_model_3": "model_3_pampa_prob"})
-        df[["SMILES", "model_0_pampa_prob", "model_1_pampa_prob", "model_2_pampa_prob", "model_3_pampa_prob"]].to_csv(f"{dset}_chemeleon_pampa_preds_individual.csv", index=False)
-    
